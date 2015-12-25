050365f763692da64eed81e0032ff63804f87710
Jason Shipman <jasonpshipman@gmail.com>
2015-12-25 01:10:54 -0500

    Use Word32 instead of Int in puzzle data.

--- lib/GCHQ/Data/Puzzle.hs
+++ lib/GCHQ/Data/Puzzle.hs
@@ -13,14 +13,14 @@ import Data.Aeson ( FromJSON, ToJSON, decode', encode )
 import Data.Bits ( (.|.), shiftL )
 import qualified Data.ByteString.Lazy.Char8 as LBS ( ByteString )
 import Data.Foldable ( foldl' )
-import Data.List ( group, groupBy, nub, sort )
-import Data.Tuple ( swap )
+import Data.List ( genericLength, group, groupBy, nub, sort )
+import Data.Word ( Word32 )
 import GHC.Generics ( Generic )
 
 data Puzzle = Puzzle
-  { shadedSquares       :: [(Int, Int)]
-  , rowShadingSequences :: [[Int]]
-  , colShadingSequences :: [[Int]]
+  { shadedSquares       :: [(Word32, Word32)]
+  , rowShadingSequences :: [[Word32]]
+  , colShadingSequences :: [[Word32]]
   } deriving ( Generic, Show )
 
 instance FromJSON Puzzle
@@ -49,9 +49,9 @@ readPuzzleJSON bs = decode' bs >>= check >>= prepare where
     checkRowColCountsEqual = curry (length *** length >>> uncurry (==))
     checkSeqItemsArePositive = and . map (all (> 0))
     checkShadingSeqsVsBounds shadingSeqs = and $
-      map (sum &&& length >>> uncurry (+) >>> pred >>> (<= length shadingSeqs)) shadingSeqs
+      map (sum &&& genericLength >>> uncurry (+) >>> pred >>> (<= genericLength shadingSeqs)) shadingSeqs
     checkShadedSquares shadingSeqs = and . map (checkIndex *** checkIndex >>> uncurry (&&)) where
-      checkIndex = (>= 0) &&& (< length shadingSeqs) >>> uncurry (&&)
+      checkIndex = (>= 0) &&& (< genericLength shadingSeqs) >>> uncurry (&&)
 
   prepare p@(Puzzle ss _ _) = Just $ p { shadedSquares = sort . nub $ ss }
 
@@ -64,20 +64,21 @@ solvePuzzle inPuzzle@(Puzzle inSs inRss inCss) = outPuzzle where
   newShadedSquares = [(0, 0)]
 
   allValidRowInts = map (map snd) $ flip map allRowInts $ filter $
-    second (BA.BitArray >>> BA.toBoolList >>> group >>> filter (all (== True)) >>> map length) >>> uncurry (==)
+    second (BA.BitArray >>> BA.toBoolList >>> group >>> filter (all (== True)) >>> map genericLength) >>> uncurry (==)
   allRowInts = map (first repeat >>> second (flip enumFromTo (2 ^ rowColCount - 1)) >>> uncurry zip)
              . zip inRss
              $ initRowInts
 
   initRowInts = reverse . (lastZeroes ++) . foldl' (\acc (pos, val) ->
-    val : (replicate (pos - length acc) 0) ++ acc) [] $ rowToInitIntPairs
+    val : (replicate (fromIntegral (pos - genericLength acc)) 0) ++ acc) [] $ rowToInitIntPairs
 
-  lastZeroes = flip replicate 0 $ rowColCount - (fst $ last rowToInitIntPairs) - 1
+  lastZeroes = flip replicate 0
+             $ fromIntegral rowColCount - (fromIntegral . fst . last $ rowToInitIntPairs) - 1
 
   rowToInitIntPairs = map (foldr1 $ curry $ fst &&& snd *** snd >>> fst *** uncurry (.|.)) rowToSetBitPairs
-  rowToSetBitPairs = map (map $ second (shiftL (1 :: Int))) shadedSquaresGroupedByRow
+  rowToSetBitPairs = map (map $ second (fromIntegral >>> shiftL (1 :: Word32))) shadedSquaresGroupedByRow
   shadedSquaresGroupedByRow = groupBy (curry $ fst *** fst >>> uncurry (==)) inSs
 
-  rowColCount = length inRss
+  rowColCount :: Word32; rowColCount = genericLength inRss
 
   assocPairLeft  (a, (b, c)) = ((a, b), c)
