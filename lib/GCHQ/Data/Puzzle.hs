a90fbf56637451e77c0bdbed95a8558934ccfeb3
Jason Shipman <jasonpshipman@gmail.com>
2015-12-27 22:29:48 -0500

    Some initial experimentation using conduit.

--- lib/GCHQ/Data/Puzzle.hs
+++ lib/GCHQ/Data/Puzzle.hs
@@ -5,15 +5,19 @@ module GCHQ.Data.Puzzle
   , readPuzzleJSON
   , writePuzzleJSON
   , solvePuzzle
+  , solvePuzzle2
   ) where
 
 import qualified BitArray as BA ( BitArray(..), toBoolList )
+import Conduit
 import Control.Arrow ( (&&&), (***), (>>>), first, second )
+import Control.Monad.Identity ( Identity )
 import Data.Aeson ( FromJSON, ToJSON, decode', encode )
 import Data.Bits ( (.|.), shiftL )
 import qualified Data.ByteString.Lazy.Char8 as LBS ( ByteString )
 import Data.Foldable ( foldl' )
 import Data.List ( genericLength, find, group, groupBy, nub, sort, transpose )
+import Data.Tuple ( swap )
 import Data.Word ( Word32 )
 import GHC.Generics ( Generic )
 
@@ -88,3 +92,43 @@ solvePuzzle (Puzzle inSs inRss inCss) = outPuzzle where
 
   rowColCount = (genericLength inRss) :: Word32
   natWords = [(0 :: Word32)..]
+
+solvePuzzle2 :: Puzzle -> IO ()
+solvePuzzle2 (Puzzle inSs inRss inCss) = runConduit solver where
+  solver = shadedSquaresGroupedByRow
+       =$= rowToSetBitPairs
+       =$= rowToInitIntPairs
+       =$= initRowInts
+       =$= allRowInts
+       =$= takeC 1
+       =$= printC
+
+  shadedSquaresGroupedByRow :: Source IO [(Word32, Word32)]
+  shadedSquaresGroupedByRow = yieldMany
+                            . groupBy (curry $ fst *** fst >>> uncurry (==))
+                            $ inSs
+
+  rowToSetBitPairs :: Conduit [(Word32, Word32)] IO [(Word32, Word32)]
+  rowToSetBitPairs = mapCE (second $ (fromIntegral >>> shiftL (1 :: Word32)))
+
+  rowToInitIntPairs :: Conduit [(Word32, Word32)] IO (Word32, Word32)
+  rowToInitIntPairs = mapC
+                    $ foldr1
+                    $ curry (fst &&& snd *** snd >>> fst *** uncurry (.|.))
+
+  initRowInts :: Conduit (Word32, Word32) IO Word32
+  initRowInts = do
+    flip concatMapAccumC 0 $ \(i, v) acc ->
+      (1 + i, replicate (fromIntegral (i - acc)) 0 ++ [v])
+    yieldMany $ flip replicate 0 $ fromIntegral (rowColCount - lastRowWithHints)
+
+  allRowInts :: Conduit Word32 IO [Word32]
+  allRowInts = awaitForever $ yield . flip enumFromTo (2 ^ rowColCount - 1)
+
+--allValidRowInts = map (map snd) $ flip map allRowInts $ filter $ second (toBools >>> toSequenceLengths) >>> uncurry (==)
+
+  rowColCount :: Word32
+  rowColCount = genericLength inRss
+
+  lastRowWithHints :: Word32
+  lastRowWithHints = last >>> fst $ inSs
--- solver/Main.hs
+++ solver/Main.hs
@@ -4,7 +4,7 @@ module Main where
 
 import qualified Data.ByteString.Lazy as LBS ( readFile, writeFile )
 import Data.Maybe ( listToMaybe )
-import GCHQ.Data.Puzzle ( readPuzzleJSON, solvePuzzle, writePuzzleJSON )
+import GCHQ.Data.Puzzle ( readPuzzleJSON, solvePuzzle, solvePuzzle2, writePuzzleJSON )
 import System.Environment ( getArgs )
 
 main :: IO ()
@@ -14,8 +14,9 @@ main = listToMaybe <$> getArgs >>= run where
     inputPuzzleJSON  <- LBS.readFile filePath
     case readPuzzleJSON inputPuzzleJSON of
       Nothing -> putStrLn $ "ERROR: Failed to parse JSON: " ++ filePath
-      Just inputPuzzle -> case solvePuzzle inputPuzzle of
-        Nothing -> putStrLn $ "ERROR: Failed to solve puzzle: " ++ filePath
-        Just solvedPuzzle -> LBS.writeFile ("solution.json")
-                           . writePuzzleJSON
-                           $ solvedPuzzle
+      --Just inputPuzzle -> case solvePuzzle inputPuzzle of
+      --  Nothing -> putStrLn $ "ERROR: Failed to solve puzzle: " ++ filePath
+      --  Just solvedPuzzle -> LBS.writeFile ("solution.json")
+      --                     . writePuzzleJSON
+      --                     $ solvedPuzzle
+      Just inputPuzzle -> solvePuzzle2 inputPuzzle
