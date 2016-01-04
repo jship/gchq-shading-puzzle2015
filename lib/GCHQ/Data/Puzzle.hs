0be7baa1f595aa608a18c48d47c1009db5bbb521
Jason Shipman <jasonpshipman@gmail.com>
2016-01-03 21:55:49 -0500

    Implement renderer executable.

--- lib/GCHQ/Data/Puzzle.hs
+++ lib/GCHQ/Data/Puzzle.hs
@@ -2,20 +2,35 @@
 
 module GCHQ.Data.Puzzle
   ( Puzzle
+  , mapPuzzleSquares
   , readPuzzleJSON
+  , rowColCount
   , writePuzzleJSON
   , solvePuzzle
   ) where
 
-import GCHQ.Data.Puzzle.Internal
-  ( Puzzle, checkPuzzle, preparePuzzle, runSATSolver )
 import Control.Monad ( (>=>) )
 import Data.Aeson ( decode', encode )
 import qualified Data.ByteString.Lazy.Char8 as LBS ( ByteString )
 import Data.Either ( Either )
+import Data.Function ( (.), ($) )
+import Data.Functor ( fmap )
+import Data.Foldable ( foldr )
+import Data.Ix ( range )
+import Data.List ( insert, repeat, zip )
 import Data.Maybe ( Maybe )
 import Data.String ( String )
-import Prelude ( IO )
+import Data.Word ( Word8 )
+import GCHQ.Data.Puzzle.Internal
+  ( Puzzle(..), checkPuzzle, preparePuzzle, gridRange, rowColCount
+  , runSATSolver )
+import Prelude ( Bool(..), IO )
+
+mapPuzzleSquares :: (((Word8, Word8), Bool) -> a) -> Puzzle -> [a]
+mapPuzzleSquares f p@(Puzzle ss _ _) = fmap f finalSquareList where
+  finalSquareList = foldr insert initSquareList shadedSquareList
+  initSquareList = zip (range . gridRange $ p) (repeat False)
+  shadedSquareList = zip ss (repeat True)
 
 readPuzzleJSON :: LBS.ByteString -> Maybe Puzzle
 readPuzzleJSON = decode' >=> checkPuzzle >=> preparePuzzle
--- renderer/Main.hs
+++ renderer/Main.hs
@@ -1,2 +1,50 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+import qualified Data.ByteString.Lazy as LBS ( readFile )
+import Data.Maybe ( listToMaybe )
+import Data.Word ( Word8 )
+import GCHQ.Data.Puzzle
+  ( Puzzle, mapPuzzleSquares, readPuzzleJSON, rowColCount )
+import Graphics.Gloss
+import System.Environment ( getArgs )
+
+windowConfig :: Display
+windowConfig = InWindow "Puzzle" (winWidth, winWidth) (10, 10)
+
+winWidth :: Int
+winWidth = 400
+
+backgroundColor :: Color
+backgroundColor = violet
+
+gridPic :: Puzzle -> Picture
+gridPic puzzle = rotate 90.0
+               . translate gridTran gridTran
+               . pictures
+               $ squarePics where
+  squarePics = mapPuzzleSquares toPic puzzle
+  toPic :: (((Word8, Word8), Bool) -> Picture)
+  toPic ((row, col), isShaded) = pic where
+    pic = translate tranX tranY
+        . color rectColor
+        $ rectangleSolid squareEdgeLength squareEdgeLength
+    tranX, tranY :: Float
+    tranX = fromIntegral row * squareEdgeLength
+    tranY = fromIntegral col * squareEdgeLength
+    rectColor = if isShaded then black else white
+  squareEdgeLength = fromIntegral winWidth / fromIntegral squaresPerSide
+  squaresPerSide = rowColCount puzzle
+  gridTran = (-0.5 * fromIntegral winWidth) + (0.5 * squareEdgeLength)
+
 main :: IO ()
-main = putStrLn "Renderer not implemented!"
+main = listToMaybe <$> getArgs >>= run where
+  run Nothing = fail "ERROR: Must specify input file"
+  run (Just filePath) = do
+    inputPuzzleJSON  <- LBS.readFile filePath
+    case readPuzzleJSON inputPuzzleJSON of
+      Nothing -> fail $ "ERROR: Failed to parse JSON: " ++ filePath
+      Just inputPuzzle -> display windowConfig backgroundColor
+                        . gridPic
+                        $ inputPuzzle
